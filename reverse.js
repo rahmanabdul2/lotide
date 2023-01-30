2d27d865a455fea7cad2fd422ad42146ad2675ff
rahmanabdul2 <rahmanshahram@gmail.com>
2023-01-30 14:45:43 -0500

    added reverse.js with two solutions

---
+++ reverse.js
@@ -0,0 +1,26 @@
+const args = process.argv.slice(2);
+
+
+
+////// without the use of built-in functions
+const reverseString = function(args) {
+  let reversed = "";
+  for (let i = args.length - 1; i >= 0; i--) {
+    reversed += args[i];
+  }
+  return reversed;
+};
+console.log(reverseString("hey how are you"));
+
+
+
+
+
+
+////////// Using the prototype functions
+
+const reverseStr = function(str) {
+  return str.split('').reverse().join('');
+};
+
+console.log(reverseStr());
