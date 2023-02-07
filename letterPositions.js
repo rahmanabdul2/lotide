0e5db7dbb5bd15a2605c7df6d8dcc966aeda3b99
rahmanabdul2 <rahmanshahram@gmail.com>
2023-02-07 15:11:33 -0500

    added the letterPosition.js function that returns an object

---
+++ letterPositions.js
@@ -0,0 +1,21 @@
+// will return all the indices (zero-based positions) in the string where each character is found.
+// multiple numbers may be needed to represent all the places in the string that it shows up.
+
+const letterPositions = function(str) {
+  const results = {};
+  // Loop thru the str letters
+  for (var i = 0; i < str.length; i++) {
+    if (str[i] !== ' ') {
+      if (!results[str[i]]) {    // if it does not exist, assign it to a new array
+        results[str[i]] = [];
+        // push
+        results[str[i]].push(i);
+      } else {
+        results[str[i]].push(i); // if it does exist, then push it to the exisiting one.
+      }
+    }
+  }
+  return results;
+};
+
+console.log(letterPositions('lighthouse in the house'));
