2db0f26a31ff6cf1d0dc7c5eb458a52cce001e13
rahmanabdul2 <rahmanshahram@gmail.com>
2023-02-07 18:08:15 -0500

    corrected the eqObjects.js functions with Test Codes

--- eqObjects.js
+++ eqObjects.js
@@ -14,6 +14,7 @@ const eqObjects = function(obj1, obj2) {
   // converts the objects keys into array
   const arrKeys1 = Object.keys(obj1);
   const arrKeys2 = Object.keys(obj2);
+  let result = true;
 
   // if they have the same number of keys
   if (arrKeys1.length !== arrKeys2.length) {
@@ -26,7 +27,7 @@ const eqObjects = function(obj1, obj2) {
       return false;
     }
   }
-  return true;
+  return result;
 };
 
 
@@ -34,3 +35,7 @@ const eqObjects = function(obj1, obj2) {
 const ab = { a: "1", b: "2", c: '4' };
 const ba = { b: "2", a: "1", c: '5', };
 assertEqual(eqObjects(ab, ba), false); // Passed (fales === false)
+
+const shirtObject = { color: "red", size: "medium" };
+const anotherShirtObject = { size: "medium", color: "red" };
+assertEqual(eqObjects(shirtObject, anotherShirtObject), true); // => true
