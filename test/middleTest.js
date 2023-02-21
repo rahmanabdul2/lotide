7e36e08de4db6ead4911cdbb49bf8f0db5837015
rahmanabdul2 <rahmanshahram@gmail.com>
2023-02-21 00:49:19 -0500

    incorporated mocha and chai to test the middle function

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,16 +1,18 @@
 const middle = require('../middle'); // returns the expected in an array
-const assertArraysEqual = require('../assertArraysEqual'); // not going to need this anymore
-// const assert = require('chai').assert;
+const assert = require('chai').assert;
 
 
 
+describe("#Codes That Pass", () => {
+  it("expect [25, 30] to return for '[10, 25, 30, 40]'", () => {
+    assert.deepEqual(middle([10, 25, 30, 40]), [25, 30]); // The assert.deepEqual compares objects and arrays | Pass
+  });
+  it("expect ['Lighthouse'] to return for '['Hello', 'Lighthouse', 'Labs']'", () => {
+    assert.deepEqual(middle(['Hello', 'Lighthouse', 'Labs']), ['Lighthouse']); // Pass
+  });
+  it("expected [10, 25, 30, 40, 60] to be different than [30]", () => {
+    assert.notDeepEqual(middle([10, 25, 30, 40, 60]), [40]); // notEqual (notDeepEqual) will FAIL if actual EQUALs expected; it will PASS if not equal
+  });
+});
 
 
-
-
-
-////////////////////////////// TEST CODES
-assertArraysEqual(middle([10, 25, 30, 40]), [25, 30]); // PASS
-assertArraysEqual(middle([10, 25, 30, 40, 60]), [25]); // FAIL
-assertArraysEqual(middle(['Hello', 'Lighthouse', 'Labs']), ['Lighthouse']); // PASS
-assertArraysEqual(middle([10, 25, 30, 40]), [25, 10, 30, 40]); // FAIL
